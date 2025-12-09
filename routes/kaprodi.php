126410f0241aece39435dfc7d32214169b56cc09
Maulan Fawaidzul Amarin <maulanamarin0@gmail.com>
2025-12-09 19:48:36 +0700

    ralat

--- routes/kaprodi.php
+++ routes/kaprodi.php
@@ -1,13 +1,29 @@
 <?php
-
+use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Kaprodi\DashboardController;
+use App\Http\Controllers\Kaprodi\KurikulumController;
 
-// Semua route WAREK I pakai prefix /warek1 dan harus login
-Route::prefix('kaprodi')->middleware(['auth'])->group(function () {
 
-    // Dashboard WAREK I
-    Route::get('/dashboard', [DashboardController::class, 'index'])
-        ->name('kaprodi.dashboard');
+Route::middleware('auth')->prefix('kaprodi')->group(function () {
+
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('kaprodi.dashboard.index');
+    })->name('kaprodi.dashboard');
+
+    // ===============================
+    // Kurikulum 
+    // ===============================
+    Route::get('/kurikulum', [KurikulumController::class, 'index'])->name('kurikulum.index');
+    Route::get('/kurikulum/create', [KurikulumController::class, 'create'])->name('kurikulum.create');
+    Route::post('/kurikulum', [KurikulumController::class, 'store'])->name('kurikulum.store');
+    Route::get('/kurikulum/{id}/edit', [KurikulumController::class, 'edit'])->name('kurikulum.edit');
+    Route::put('/kurikulum/{id}', [KurikulumController::class, 'update'])->name('kurikulum.update');
+    Route::delete('/kurikulum/{id}', [KurikulumController::class, 'destroy'])->name('kurikulum.destroy');
+
+    
+
+
+    
 
 });
